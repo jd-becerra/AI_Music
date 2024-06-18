a3c800f5413a27f9411d11625a4feadb85d8253b
jd-becerra <jd-becerra>
2024-06-18 01:59:59 -0600

    Added libraries and Python venv

---
+++ test_install.py
@@ -0,0 +1,19 @@
+'''
+Import these modules
+fluidsynth
+midi2audio
+keras
+numpy
+pandas
+music21
+pretty_midi
+seaborn
+tensorflow
+matplotlib
+typing
+IPython
+'''
+
+import fluidsynth, midi2audio, keras, numpy, pandas, music21, pretty_midi, seaborn, tensorflow, matplotlib, typing, IPython, os, sys, collections
+
+print("All modules are installed successfully!")
